8cbcef788081a91a2993d094522cc02094e1dfca
GamaRais <75371289+GamaRais@users.noreply.github.com>
2025-03-06 04:44:52 +0300

    Add files via upload

---
+++ soft.js
@@ -0,0 +1,25 @@
+//softboiled egg
+const startingMinutes = 4;
+let time = startingMinutes * 60;
+var audio = new Audio('audio/Morning_Alarm.MP3')
+
+const countdownEl = document.getElementById('softCountdown');
+
+myInterval = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time/60)
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    if (time <= 0) {
+        clearInterval(myInterval);
+        countdownEl.innerHTML = "Your eggs are done!"
+        audio.play();
+        audio.volume = 0.07;
+        audio.loop()
+    }
+    time--;
+}
